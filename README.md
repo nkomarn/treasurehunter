f20814b48b08f125d073dece3844605a59169eae
Elian <elian1203@gmail.com>
2017-06-05 09:26:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TreasureHunter
+Spawns chests around the world at intervals containing
+various loot.
+
+# Server
+http://urbanmc.net
+
+# Developers
+Elian, Silverwolfg11
---
+++ src/net/urbanmc/treasurehunter/TreasureHunter.java
@@ -0,0 +1,16 @@
+package net.urbanmc.treasurehunter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TreasureHunter extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}

f8cd820b41c4279a79a8354ef637081146548c9d
Silverwolfg11 <silverwolfg11@gmail.com>
2020-03-04 19:40:04 -0800

    Add leather armor color support

--- src/main/java/net/urbanmc/treasurehunter/util/ItemUtil.java
+++ src/main/java/net/urbanmc/treasurehunter/util/ItemUtil.java
@@ -2,6 +2,7 @@ package net.urbanmc.treasurehunter.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -9,6 +10,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -18,6 +20,26 @@ import java.util.List;
 import java.util.logging.Level;
 
 public class ItemUtil {
+
+	public static List<ItemStack> getItemList(List<String> list, FileConfiguration data) {
+		List<ItemStack> items = new ArrayList<>();
+
+		for (String s : list) {
+			ItemStack stack = null;
+
+			try {
+				stack = getItem(s, data);
+			} catch (Exception e) {
+				Bukkit.getLogger().warning("[TreasureHunter] Exception thrown while parsing item line " + s);
+				e.printStackTrace();
+			}
+
+			if (stack != null)
+				items.add(stack);
+		}
+
+		return items;
+	}
 
 	private static ItemStack getItem(String name, FileConfiguration data) {
 		String[] split = name.split(" ");
@@ -36,7 +58,9 @@ public class ItemUtil {
 
 		ItemMeta meta = is.getItemMeta();
 
-		for (String arg : split) {
+		for (int i = 1; i < split.length; ++i) {
+			String arg = split[i];
+
 			if (arg.startsWith("name:")) {
 				String displayName = ChatColor.translateAlternateColorCodes('&', arg.substring(5).replace("_", " "));
 				meta.setDisplayName(ChatColor.RESET + displayName);
@@ -117,15 +141,90 @@ public class ItemUtil {
 				short durability = Short.parseShort(arg.substring(11));
 				is.setDurability(durability);
 			}
-			else {
-				// Log unknown argument
-				Bukkit.getLogger().warning("[TreasureHunter] Unknown argument encountered: " + arg + " while parsing item " + name);
+			else if (arg.startsWith("color:")) {
+				handleColor(is, arg.substring(6));
 			}
+			// Log unknown argument
+			Bukkit.getLogger().warning("[TreasureHunter] Unknown argument encountered: " + arg + " while parsing item " + name);
 		}
 
 		is.setItemMeta(meta);
 
 		return is;
+	}
+
+	private static void handleColor(ItemStack is, String colorString) {
+		ItemMeta meta = is.getItemMeta();
+		if (is.getItemMeta() instanceof LeatherArmorMeta) {
+			Color color;
+
+			if (colorString.charAt(0) == '#')
+				color = colorFromHex(colorString.substring(1));
+			else
+				color = fromColorName(colorString);
+
+			((LeatherArmorMeta) meta).setColor(color);
+			is.setItemMeta(meta);
+		}
+		else {
+			Bukkit.getLogger().warning("[TreasureHunter] Cannot apply leather armor color to material type: "
+					+ is.getType().name() + "!");
+		}
+	}
+
+	private static Color colorFromHex(String hex) {
+		try {
+			return Color.fromRGB(
+					Integer.valueOf(hex.substring(1, 3), 16),
+					Integer.valueOf(hex.substring(3, 5), 16),
+					Integer.valueOf(hex.substring(5, 7), 16)
+			);
+		} catch (NumberFormatException ex) {
+			Bukkit.getLogger().warning("[TreasureHunter] Could not parse hexadecimal rgb value for " + hex + "!");
+			return Color.WHITE;
+		}
+	}
+
+	private static Color fromColorName(String colorName) {
+		switch (colorName.toLowerCase()) {
+			case "aqua":
+				return Color.AQUA;
+			case "fuchsia":
+				return Color.FUCHSIA;
+			case "black":
+				return Color.BLACK;
+			case "blue":
+				return Color.BLUE;
+			case "gray":
+				return Color.GRAY;
+			case "green":
+				return Color.GREEN;
+			case "lime":
+				return Color.LIME;
+			case "maroon":
+				return Color.MAROON;
+			case "navy":
+				return Color.NAVY;
+			case "olive":
+				return Color.OLIVE;
+			case "yellow":
+				return Color.YELLOW;
+			case "orange":
+				return Color.ORANGE;
+			case "purple":
+				return Color.PURPLE;
+			case "red":
+				return Color.RED;
+			case "silver":
+				return Color.SILVER;
+			case "teal":
+				return Color.TEAL;
+			case "white":
+				return Color.WHITE;
+		}
+
+		Bukkit.getLogger().warning("[TreasureHunter] Error parsing leather armor color for color name: " + colorName);
+		return Color.WHITE;
 	}
 
 	private static Enchantment getEnchantment(String enchantmentName) {
@@ -155,27 +254,6 @@ public class ItemUtil {
 				return "protection_environmental";
 		}
 		return s;
-	}
-
-
-	public static List<ItemStack> getItemList(List<String> list, FileConfiguration data) {
-		List<ItemStack> items = new ArrayList<>();
-
-		for (String s : list) {
-			ItemStack stack = null;
-
-			try {
-				stack = getItem(s, data);
-			} catch (Exception e) {
-				Bukkit.getLogger().warning("[TreasureHunter] Exception thrown while parsing item line " + s);
-				e.printStackTrace();
-			}
-
-			if (stack != null)
-				items.add(stack);
-		}
-
-		return items;
 	}
 
 }

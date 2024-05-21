c0fb234a23a14537d70655befede83d19404379a
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2024-05-21 11:35:16 -0700

    feat: load messages bundle from file, if present

--- src/main/java/net/urbanmc/treasurehunter/manager/Messages.java
+++ src/main/java/net/urbanmc/treasurehunter/manager/Messages.java
@@ -1,11 +1,18 @@
 package net.urbanmc.treasurehunter.manager;
 
+import net.urbanmc.treasurehunter.TreasureHunter;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 public class Messages {
+	private static final String BUNDLE_NAME = "messages";
 	private static Messages instance = new Messages();
 
 	private ResourceBundle bundle;
@@ -23,6 +30,20 @@ public class Messages {
 	}
 
 	private void loadBundle() {
+		File dataDirectory = JavaPlugin.getPlugin(TreasureHunter.class).getDataFolder();
+		File resource = new File(dataDirectory, BUNDLE_NAME + ".properties");
+
+		if (resource.exists()) {
+			try {
+				bundle = new PropertyResourceBundle(new FileInputStream(resource));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+			return;
+		}
+
+		/* fall back to included messages bundle */
 		bundle = ResourceBundle.getBundle("messages");
 	}
 

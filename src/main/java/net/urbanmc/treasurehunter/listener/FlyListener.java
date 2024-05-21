787b4e6683157c01102e40c6a14fdebe69a5818c
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2024-05-21 11:25:16 -0700

    feat: prevent use of elytras while hunting

--- src/main/java/net/urbanmc/treasurehunter/listener/FlyListener.java
+++ src/main/java/net/urbanmc/treasurehunter/listener/FlyListener.java
@@ -5,8 +5,13 @@ import net.urbanmc.treasurehunter.manager.Messages;
 import net.urbanmc.treasurehunter.manager.TreasureChestManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 public class FlyListener implements Listener {
 
@@ -31,4 +36,24 @@ public class FlyListener implements Listener {
 		e.setCancelled(true);
 	}
 
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void useElytra(EntityToggleGlideEvent event) {
+		if (!(event.getEntity() instanceof Player player))
+			return;
+
+		if (TreasureChestManager.getInstance().getCurrentChest() == null)
+			return;
+
+		if (!TreasureChestManager.getInstance().getCurrentChest().isHunting(player))
+			return;
+
+		EntityEquipment equipment = player.getEquipment();
+		ItemStack elytraItem = equipment.getItem(EquipmentSlot.CHEST);
+		equipment.setItem(EquipmentSlot.CHEST, null);
+
+		/* add the elytra to the player's inventory, or drop it on the ground */
+		player.getInventory().addItem(elytraItem).forEach((index, stack) -> {
+			player.getWorld().dropItemNaturally(player.getLocation(), elytraItem).setOwner(player.getUniqueId());
+		});
+	}
 }
